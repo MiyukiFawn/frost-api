99c3c6ecb36322398a7e3a143fc56ca64e82c2ae
Miyuki <79578176+MiyukiFawn@users.noreply.github.com>
2023-08-27 17:23:02 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -45,7 +45,7 @@ app.use((req: Request, res: Response, next: NextFunction) => {
   res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT");
   res.setHeader(
     "Access-Control-Allow-Headers",
-    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization"
   );
 
   next();
